a60288a97837822cfa8cf1f6b10306236ec7ff71
sputurz <sputurza@gmail.com>
2025-10-26 06:00:37 +0300

    26.10.25h6

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -1,15 +1,15 @@
-// import styles from './Hero.module.scss';
+import styles from './Hero.module.scss';
 import { useEffect, useRef } from 'react';
 import './SimpleParallaxHero.css';
 
 export function Hero() {
-  const parallaxRef = useRef<HTMLDivElement>(null);
+  const bgRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (parallaxRef.current) {
-        const offset = window.pageYOffset;
-        parallaxRef.current.style.backgroundPositionY = `${offset * 0.8}px`;
+      if (bgRef.current) {
+        const offset = window.scrollY;
+        bgRef.current.style.backgroundPositionY = `${offset * 0.8}px`;
       }
     };
 
@@ -19,10 +19,30 @@ export function Hero() {
 
   return (
     <>
-      <div className="parallax-container">
-        <div ref={parallaxRef} className="parallax"></div>
-        <div className="overlay1"></div>
-      </div>
+      <section className={styles.hero}>
+        <div ref={bgRef} className={styles.hero__bg}></div>
+        <div className={styles.hero__overlay}></div>
+        <div className={styles.hero__wrap}>
+          <h1 className={styles.hero__title}>
+            I am Raman Mikulich.
+            <br></br> I am a Frontend
+            <br></br>developer based in Minsk.
+          </h1>
+          <div className={styles['hero__link-wrap']}>
+            <a className={styles.hero__link} href="#">
+              Latest Projects
+            </a>
+            <a className={styles.hero__link} href="#">
+              More About Me
+            </a>
+          </div>
+          <span className={styles.hero__greeting}>Hello There</span>
+          <a className={styles['hero__scrool-link']} href="#">
+            Scroll Down
+          </a>
+        </div>
+      </section>
+
       <div className="content">
         <h1>Параллакс Эффект на React + TypeScript</h1>
         <p>Прокрутите вниз, чтобы увидеть эффект.</p>
