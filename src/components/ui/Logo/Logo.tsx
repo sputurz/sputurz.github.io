041411f3165893086a83b804858b39709b9b6bb6
sputurz <sputurza@gmail.com>
2025-11-01 18:07:54 +0300

    GitFix

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import styles from './Logo.module.scss';
 export function Logo() {
   return (
     <a className={styles.logo} href="#" aria-label="Main Page">
-      <img className={styles.logo__img} src={'/logoBlack.svg'}></img>
+      <img className={styles.logo__img} src={'./logoBlack.svg'}></img>
     </a>
   );
 }
