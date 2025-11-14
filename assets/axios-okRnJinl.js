824ea5426f4ba78318fb59a556a4ae29d0b01102
sputurz <sputurza@gmail.com>
2025-11-14 04:48:00 +0300

    Updates

---
+++ assets/axios-okRnJinl.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-DHh25MGc.js";const l=e=>t.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:24,height:24,...e},t.createElement("path",{fill:"currentColor",d:"M11 2.9v19.4L9 24V7.9H5l6-5zM15 0v15.8h4l-6.2 5V1.6L15 0z"}));export{l as default};
--- assets/processing-Mi0Ohkct.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-WgLq6Ii5.js";const o=e=>t.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:800,height:800,fill:"none",...e},t.createElement("path",{stroke:"currentColor",strokeWidth:150,d:"M400 500c300 0 300-400 0-400M400 200 100 600M100 300l100 200"}));export{o as default};
