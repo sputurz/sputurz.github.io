054d381f364e127629379d5a2978b7a18c1fe1ea
sputurz <sputurza@gmail.com>
2025-11-27 02:52:54 +0300

    27.11.25h2

--- src/components/sections/About/About.tsx
+++ src/components/sections/About/About.tsx
@@ -17,32 +17,52 @@ export function About() {
           id={`${siteConfig.navLinks.about.name}-title`}
           name={siteConfig.navLinks.about.name}
           title="More About Me"
-          text="Lorem ipsum Dolor adipisicing nostrud et aute Excepteur amet commodo ea dolore irure esse Duis nulla sint fugiat cillum ullamco proident aliquip quis qui voluptate dolore veniam Ut laborum non est in officia."
+          text="A motivated, aspiring front-end developer focused on React and modern JavaScript. I have experience building SPA applications for pet projects. I'm eager to participate in commercial projects where I can apply and develop my skills, work in a team, and make a difference."
         />
         <div className={styles.about__wrap}>
-          {siteConfig.softSkills.map((skills) => (
-            <div key={skills.title} className={styles['about__skills-list']}>
-              <h3 className={styles['about__sub-title']}>{skills.title}:</h3>
-              <ShowMore
-                array={skills.list}
-                renderItem={(skill) => (
-                  <Graph
-                    title={
-                      <>
-                        <Icon
-                          name={skill.iconName}
-                          className={styles.about__svg}
-                        ></Icon>
-                        <span>{skill.name}</span>
-                      </>
-                    }
-                    tooltip={softSkillsGrades[skill.level].level}
-                    value={skill.level}
-                  ></Graph>
-                )}
-              />
-            </div>
-          ))}
+          <div>
+            <h3 className={styles['about__sub-title']}>Howdy!</h3>
+            <p className={styles.about__text}>
+              {`Aspiring frontend developer, passionate about creating modern web
+            interfaces using React and the ecosystem around it. I have an
+            engineering background and experience working with technical
+            documentation, which helps me approach tasks systematically and
+            quickly grasp new technologies. I've always been interested in IT
+            (I've programmed in Arduino, C, and Python) and plan to further
+            develop my skills in web development. I'm already proficient in
+            frontend coding and JavaScript, and am currently learning Django and
+            MySQL. I'm also exploring new and relevant CSS/HTML/JS technologies
+            on learn.javascript.ru and MDN. I also solve problems on leetcode
+            and codewars. I'm also interested in web development channels on
+            Reddit, YouTube, and Habr. I'm open to internships and
+            junior/trainee positions, eager to learn and work with others.`}
+            </p>
+          </div>
+          <ul className={styles['about__skills-list']}>
+            {siteConfig.softSkills.map((skills) => (
+              <li key={skills.title} className={styles['about__skills-item']}>
+                <h3 className={styles['about__sub-title']}>{skills.title}:</h3>
+                <ShowMore
+                  array={skills.list}
+                  renderItem={(skill) => (
+                    <Graph
+                      title={
+                        <>
+                          <Icon
+                            name={skill.iconName}
+                            className={styles.about__svg}
+                          ></Icon>
+                          <span>{skill.name}</span>
+                        </>
+                      }
+                      tooltip={softSkillsGrades[skill.level].level}
+                      value={skill.level}
+                    ></Graph>
+                  )}
+                />
+              </li>
+            ))}
+          </ul>
         </div>
       </section>
     </>
